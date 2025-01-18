09aadbaabbfa2cb3792e9bf79264872a178e7d25
Christopher Hatch <chrishatch03@gmail.com>
2025-01-18 16:16:38 -0700

    Refactored BishopMovesCalculator.

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -5,96 +5,33 @@ import java.util.Collection;
 
 public class BishopMovesCalculator implements PieceMovesCalculator {
 
-    public Collection<ChessMove> pieceMoves(ChessBoard chessBoard, ChessPosition bishopPosition) {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition bishopPosition) {
         ArrayList<ChessMove> validMoves = new ArrayList<ChessMove>();
-        ChessPiece bishop = chessBoard.getPiece(bishopPosition);
+        ChessPiece bishop = board.getPiece(bishopPosition);
+        ChessGame.TeamColor bishopColor = bishop.getTeamColor();
 
-//      get moves going up and to the right
-        ChessPosition evalPosition = new ChessPosition(bishopPosition.getRow() + 1, bishopPosition.getColumn() + 1);
-        while (ChessBoard.inBounds(evalPosition)) {
+        validMoves.addAll(getDirectionMoves(bishopPosition, bishopColor, 1, 1, board));
+        validMoves.addAll(getDirectionMoves(bishopPosition, bishopColor, -1, 1, board));
+        validMoves.addAll(getDirectionMoves(bishopPosition, bishopColor, -1, -1, board));
+        validMoves.addAll(getDirectionMoves(bishopPosition, bishopColor, 1, -1, board));
 
-            if (chessBoard.isEmpty(evalPosition)) {
-                ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                validMoves.add(newMove);
+        return validMoves;
+    }
+
+    public Collection<ChessMove> getDirectionMoves(ChessPosition bishopPosition, ChessGame.TeamColor bishopColor, int yIncrement, int xIncrement, ChessBoard board) {
+        ArrayList<ChessMove> validMoves = new ArrayList<>();
+
+        ChessPosition evalPos = new ChessPosition(bishopPosition.getRow() + yIncrement, bishopPosition.getColumn() + xIncrement);
+        while (ChessBoard.inBounds(evalPos)) {
+            if (board.isEmpty(evalPos)) {
+                validMoves.add(new ChessMove(bishopPosition, evalPos, null));
+                evalPos = new ChessPosition(evalPos.getRow() + yIncrement, evalPos.getColumn() + xIncrement);
+            } else if (board.getPiece(evalPos).getTeamColor() != bishopColor) {
+                validMoves.add(new ChessMove(bishopPosition, evalPos, null));
+                break;
             } else {
-                ChessPiece evalPiece = chessBoard.getPiece(evalPosition);
-                ChessGame.TeamColor evalTeamColor = evalPiece.getTeamColor();
-
-                if (bishop.getTeamColor() == evalTeamColor) {
-                    break;
-                } else if (bishop.getTeamColor() != evalTeamColor) {
-                    ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                    validMoves.add(newMove);
-                    break;
-                }
+                break;
             }
-            evalPosition = new ChessPosition(evalPosition.getRow() + 1, evalPosition.getColumn() + 1);
-        }
-
-//      Down and to the right
-        evalPosition = new ChessPosition(bishopPosition.getRow() - 1, bishopPosition.getColumn() + 1);
-        while (ChessBoard.inBounds(evalPosition)) {
-
-            if (chessBoard.isEmpty(evalPosition)) {
-                ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                validMoves.add(newMove);
-            } else {
-                ChessPiece evalPiece = chessBoard.getPiece(evalPosition);
-                ChessGame.TeamColor evalTeamColor = evalPiece.getTeamColor();
-
-                if (bishop.getTeamColor() == evalTeamColor) {
-                    break;
-                } else if (bishop.getTeamColor() != evalTeamColor) {
-                    ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                    validMoves.add(newMove);
-                    break;
-                }
-            }
-            evalPosition = new ChessPosition(evalPosition.getRow() - 1, evalPosition.getColumn() + 1);
-        }
-
-//      Down and to the left
-        evalPosition = new ChessPosition(bishopPosition.getRow() - 1, bishopPosition.getColumn() - 1);
-        while (ChessBoard.inBounds(evalPosition)) {
-
-            if (chessBoard.isEmpty(evalPosition)) {
-                ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                validMoves.add(newMove);
-            } else {
-                ChessPiece evalPiece = chessBoard.getPiece(evalPosition);
-                ChessGame.TeamColor evalTeamColor = evalPiece.getTeamColor();
-
-                if (bishop.getTeamColor() == evalTeamColor) {
-                    break;
-                } else if (bishop.getTeamColor() != evalTeamColor) {
-                    ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                    validMoves.add(newMove);
-                    break;
-                }
-            }
-            evalPosition = new ChessPosition(evalPosition.getRow() - 1, evalPosition.getColumn() - 1);
-        }
-
-//      Up and to the Left
-        evalPosition = new ChessPosition(bishopPosition.getRow() + 1, bishopPosition.getColumn() - 1);
-        while (ChessBoard.inBounds(evalPosition)) {
-
-            if (chessBoard.isEmpty(evalPosition)) {
-                ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                validMoves.add(newMove);
-            } else {
-                ChessPiece evalPiece = chessBoard.getPiece(evalPosition);
-                ChessGame.TeamColor evalTeamColor = evalPiece.getTeamColor();
-
-                if (bishop.getTeamColor() == evalTeamColor) {
-                    break;
-                } else if (bishop.getTeamColor() != evalTeamColor) {
-                    ChessMove newMove = new ChessMove(bishopPosition, evalPosition, null);
-                    validMoves.add(newMove);
-                    break;
-                }
-            }
-            evalPosition = new ChessPosition(evalPosition.getRow() + 1, evalPosition.getColumn() - 1);
         }
 
         return validMoves;
