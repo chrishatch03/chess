56a855fef24681938f6acd3e9e6b19d11e136ef2
Christopher Hatch <chrishatch03@gmail.com>
2025-03-03 15:53:53 -0700

    testGet user service

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -4,8 +4,6 @@ import dataaccess.UserMemoryDAO;
 import model.LoginRequest;
 import model.UserData;
 import org.junit.jupiter.api.*;
-
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -86,11 +84,9 @@ public class UserServiceTests {
             userService.register(new UserData("username", "password", "email@email.com"));
             userService.register(new UserData("secondUser", "secondPassword", "secondEmail@email.com"));
             Collection<UserData> userList = userService.listAll();
-
-            var expected = Set.of(
-                    new UserData("username", "password", "email@email.com"),
-                    new UserData("secondUser", "secondPassword", "secondEmail@email.com")
-            );
+            var expected = new HashSet<>();
+            expected.add(new UserData("username", "password", "email@email.com"));
+            expected.add(new UserData("secondUser", "secondPassword", "secondEmail@email.com"));
 
             assertEquals(expected, new HashSet<>(userList), "User sets do not match");
         } catch (ResponseException ex) {
@@ -104,18 +100,41 @@ public class UserServiceTests {
             userService.register(new UserData("username", "password", "email@email.com"));
             userService.register(new UserData("secondUser", "secondPassword", "secondEmail@email.com"));
             Collection<UserData> userList = userService.listAll();
-
-            var expected = Set.of(
-                    new UserData("username", "password", "email@email.com"),
-                    new UserData("secondUser", "secondPassword", "secondEmail@email.com"),
-                    new UserData("thirdUser", "thirdPassword", "thirdEmail@email.com")
-            );
+            var expected = new HashSet<>();
+            expected.add(new UserData("username", "password", "email@email.com"));
+            expected.add(new UserData("secondUser", "secondPassword", "secondEmail@email.com"));
+            expected.add(new UserData("thirdUser", "thirdPassword", "thirdEmail@email.com"));
 
             assertNotEquals(expected, new HashSet<>(userList), "User sets should not match");
         } catch (ResponseException ex) {
             fail("ListAll Failed: " + ex.getMessage());
         }
     }
+
+    @Test
+    void testGetPos() {
+        try {
+            UserData registeredUser = userService.register(new UserData("username", "password", "email@email.com"));
+            assertNotNull(registeredUser, "Registered user should not be null");
+            UserData retrievedUser = userService.get("username");
+            assertEquals(registeredUser, retrievedUser, "User retrieved should match user registered");
+        } catch (ResponseException ex) {
+            fail("Failed to get user: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    void testGetNeg() {
+        try {
+            UserData registeredUser = userService.register(new UserData("username", "password", "email@email.com"));
+            assertNotNull(registeredUser, "Registered user should not be null");
+            userService.get("bad username");
+            fail("Expected ResponseException for requesting bad username");
+        } catch (ResponseException ex) {
+            assertEquals(500, ex.getStatusCode(), "Should return 500 for internal server error");
+        }
+    }
+
 
     @Test
     void testClear() {
