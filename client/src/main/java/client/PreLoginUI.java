38f2f3b6b8a8b8e96c10401eaee79b193f61270a
Christopher Hatch <hatch07@byu.edu>
2025-03-20 19:41:19 -0600

    PreLoginUI first version completed. Still learning how repl works

---
+++ client/src/main/java/client/PreLoginUI.java
@@ -0,0 +1,84 @@
+package client;
+
+import java.util.Arrays;
+import exception.*;
+import server.ServerFacade;
+import model.*;
+
+public class PreLoginUI {
+    private final ServerFacade server;
+    private final String serverUrl;
+    private final Repl repl;
+
+    public PreLoginUI(String serverUrl, Repl repl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+        this.repl = repl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String... params) throws ResponseException {
+        if (params.length == 3) {
+            var username = params[0];
+            var password = params[1];
+            var email = params[2];
+            System.out.println(params);
+            return server.register(new RegisterRequest(username, password, email)).toString();
+        }
+        throw new ResponseException(400, "Expected: <username> <password> <email>");
+    }
+
+    public String login(String... params) throws ResponseException {
+        if (params.length == 2) {
+            var username = params[0];
+            var password = params[1];
+            System.out.println("Logging in: username=" + username + " password=" + password);
+            return server.login(new LoginRequest(username, password)).toString();
+        }
+        throw new ResponseException(400, "Expected: <username> <password>");
+    }
+
+    // public String signIn(String... params) throws ResponseException {
+    //     if (params.length >= 1) {
+    //         state = State.SIGNEDIN;
+    //         visitorName = String.join("-", params);
+    //         ws = new WebSocketFacade(serverUrl, notificationHandler);
+    //         ws.enterPetShop(visitorName);
+    //         return String.format("You signed in as %s.", visitorName);
+    //     }
+    //     throw new ResponseException(400, "Expected: <yourname>");
+    // }
+
+    // public String logout() throws ResponseException {
+    //     assertSignedIn();
+    //     ws.leavePetShop(userName);
+    //     ws = null;
+    //     state = State.SIGNEDOUT;
+    //     return String.format("%s left the shop", userName);
+    // }
+
+    public String help() {
+        return """
+                - register <username> <password> <email>
+                - login <username> <password>
+                - help
+                - quit
+                """;
+    }
+
+}
--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -2,21 +2,20 @@ package client;
 
 // import client.websocket.NotificationHandler;
 // import webSocketMessages.Notification;
-
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
 
 public class Repl {
-    private final PetClient client;
+    private final PreLoginUI preLoginClient;
 
     public Repl(String serverUrl) {
-        client = new PetClient(serverUrl, this);
+        preLoginClient = new PreLoginUI(serverUrl, this);
     }
 
     public void run() {
         System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
-        System.out.print(client.help());
+        System.out.print(preLoginClient.help());
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
@@ -25,14 +24,15 @@ public class Repl {
             String line = scanner.nextLine();
 
             try {
-                result = client.eval(line);
-                System.out.print(BLUE + result);
+                result = preLoginClient.eval(line);
+                System.out.print(result);
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
             }
         }
         System.out.println();
+        scanner.close();
     }
 
     // public void notify(Notification notification) {
@@ -41,7 +41,7 @@ public class Repl {
     // }
 
     private void printPrompt() {
-        System.out.print("\n" + RESET + ">>> " + GREEN);
+        System.out.print("\n" + SET_TEXT_COLOR_BLACK + ">>> " + SET_TEXT_COLOR_GREEN);
     }
 
 }
